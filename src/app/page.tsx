3dbe91d7f5d4dacf1d17d3d35bd223ddc8012034
Donny Satriya <ssatriya@gmail.com>
2024-02-25 21:25:12 +0800

    add button to admin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="max-w-7xl flex items-center justify-center min-h-screen">
-      <h1>Hello</h1>
+    <div className="w-full flex items-center justify-center min-h-screen bg-zinc-700">
+      <Button onClick={() => router.push("/admin")}>Go to admin page</Button>
     </div>
   );
 }
